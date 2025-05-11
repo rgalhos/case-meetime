3568edf6fcb3998f952baa9d81bfb59cd4caab87
Rafael Galhós <rgmg@ic.ufal.br>
2025-05-11 09:29:56 -0300

    wip: Cycle selection modal

--- src/app/components/new-entities-modal/new-entities-modal.component.ts
+++ src/app/components/new-entities-modal/new-entities-modal.component.ts
@@ -1,27 +1,36 @@
-import { Component, inject } from '@angular/core';
-import { MatButton } from '@angular/material/button';
-import { MatCard, MatCardTitle, MatCardContent } from '@angular/material/card';
+import { Component, inject, signal } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import {
   MatDialogContent,
   MatDialogRef,
   MatDialogTitle,
 } from '@angular/material/dialog';
+import { MatDivider } from '@angular/material/divider';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'new-entities-modal',
   templateUrl: './new-entities-modal.component.html',
   styleUrl: './new-entities-modal.component.scss',
   imports: [
-    MatButton,
+    MatCardModule,
     MatDialogTitle,
     MatDialogContent,
-    MatCard,
-    MatCardTitle,
-    MatCardContent,
+    MatDivider,
+    MatExpansionModule,
+    MatTreeModule,
+    MatIcon,
   ],
 })
 export class NewEntitiesModal {
   readonly dialogRef = inject(MatDialogRef<this>);
+  readonly cycleSelectionOpen = signal(false);
+
+  toggleCycleSelection() {
+    this.cycleSelectionOpen.set(!this.cycleSelectionOpen());
+  }
 
   closeDialog() {
     this.dialogRef.close();
